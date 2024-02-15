3ff4f6ff5fec2ad8f579366afffcd3c5ada55756
JR <zorael@gmail.com>
2024-02-15 12:12:29 +0100

    Add ddoc frontpage
    Move the module-level ddoc from main.d to package.d.

--- source/wg_monitor/main.d
+++ source/wg_monitor/main.d
@@ -1,15 +1,5 @@
 /**
-    Wireguard peer monitor.
-
-    Calls a Wireguard command to get the latest handshake timestamps of all peers.
-
-    A notification is sent if a peer hasn't been seen for a while, occassionally
-    as a reminder, and/or if a peer returns after having been lost.
-    Notifications can be sent via [Batsign](https://batsign.me), or by invoking
-    a custom command.
-
-    See_Also:
-        https://batsign.me
+    Main module.
 
     Copyright: [JR](https://github.com/zorael)
     License: [Boost Software License 1.0](https://www.boost.org/users/license.html)
