9f75d5c36add2f79cfca0d6e84c3569de010ddd6
JR <zorael@gmail.com>
2024-02-23 16:24:08 +0100

    Use as-gui-user.sh in notify-send.sh

--- notify-send.sh
+++ notify-send.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-# Simple wrapper around notify-send to send notifications to a user's desktop.
+# Calls notify-send to send notifications to all local graphical environments.
 # This is not easily done from a different user (root), hence this script.
 
 title="wg-monitor"
+icon="network-wireless-disconnected"
+urgency="normal"
 
-#display=":$(ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | head -n 1)"
-display=":0"  # change as necessary
-user=$(who | grep "($display)" | awk '{ print $1 }' | head -n 1)
-uid=$(id -u $user)
-
-/usr/bin/sudo -u $user \
-    DISPLAY=$display \
-    DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus \
+./as-gui-user.sh \
     /usr/bin/notify-send \
-    "$title" "$1"
+    --icon="$icon" \
+    --urgency="$urgency" \
+    "$title" \
+    "$1"
