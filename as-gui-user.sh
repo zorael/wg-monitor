24944db740843a8a999399678a2c966b6552b01c
JR <zorael@gmail.com>
2024-02-23 16:23:18 +0100

    Add as-gui-user.sh

---
+++ as-gui-user.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+# Simple helper to run a command as all users that are currently running a
+# graphical environment, with proper DBUS session environment variables set.
+# This is useful for sending desktop notifications and works with notify-send.
+
+call_as_user()
+{
+    local user="$1"
+    local display="$2"
+    local uid="$(id -u $user)"
+    shift 2
+
+    /usr/bin/sudo \
+        --user=$user \
+        DISPLAY=$display \
+        DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus \
+        "$@"
+}
+
+if [ $# -eq 0 ]; then
+    echo "usage: ${0##*/} [command] [args...]"
+    exit 0
+fi
+
+all_displays=( $(ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##') )
+
+for display_num in ${all_displays[@]}; do
+    display=":$display_num"
+    user=$(who | grep "($display)" | awk '{ print $1 }' | head -n 1)
+    [ "$user" ] || continue
+    call_as_user $user $display "$@"
+done
