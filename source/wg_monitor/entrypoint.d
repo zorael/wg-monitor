20dbb82f6c2cfc65bdc0e13a0127217ab1a47127
JR <zorael@gmail.com>
2024-03-11 17:15:21 +0100

    Catch exceptions thrown by run

--- source/wg_monitor/entrypoint.d
+++ source/wg_monitor/entrypoint.d
@@ -21,12 +21,26 @@ public:
 
     Returns:
         `0` on success, non-`0` on failure, as [wg_monitor.main.run] returns.
+        If it instead throws and doesn't internally catch the exception,
+        [wg_monitor.common.ShellReturnValue.exception|ShellReturnValue.exception]
+        is returned.
 
     See_Also:
         [wg_monitor.common.ShellReturnValue]
  */
 int main(string[] args)
 {
-    import wg_monitor.main : run;
-    return int(run(args));
+    try
+    {
+        import wg_monitor.main : run;
+        return int(run(args));
+    }
+    catch (Exception e)
+    {
+        import wg_monitor.common : ShellReturnValue;
+        import std.stdio : writeln;
+
+        writeln("[!] ", e.msg);
+        return ShellReturnValue.exception;
+    }
 }
