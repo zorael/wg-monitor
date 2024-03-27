07fc976937333b68b7274f215b920fe9562f6c3d
JR <zorael@gmail.com>
2024-03-27 08:46:37 +0100

    Don't display the startup notification locally

--- notify-send.sh
+++ notify-send.sh
@@ -3,6 +3,9 @@
 # Calls notify-send to send notifications to all local graphical environments.
 # This is not easily done from a different user (root), hence this script.
 
+# Skip the initial notification on startup (main loop iteration 0).
+[[ "$2" = "0" ]] && exit 0
+
 title="wg-monitor"
 icon="network-wireless-disconnected"
 urgency="critical"
